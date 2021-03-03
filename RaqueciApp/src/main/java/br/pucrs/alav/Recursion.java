4025be2cbe006ecd5db34aa3139be2e1a85f813e
Gabriel Velloso <gg_velloso@outlook.com>
2021-03-03 20:31:18 -0300

    3 primeiras atv feitas

--- RaqueciApp/src/main/java/br/pucrs/alav/Recursion.java
+++ RaqueciApp/src/main/java/br/pucrs/alav/Recursion.java
@@ -1,5 +1,24 @@
 package br.pucrs.alav;
 
 public class Recursion {
-	
+
+    public static void main(String args[]){
+    }
+
+    public static int MultiplicaNaturais(int x, int y){
+        if (y <= 0) return 0;
+        return x + MultiplicaNaturais(x,y-1);
+    }
+
+    public static int SomaNaturais(int x, int y){
+        if(y <= 0) return x;
+        return 1 + SomaNaturais(x,y-1);
+    }
+
+    public static double SomaFracoes(int x){
+        if(x <= 1) return 1;
+        return 1/x + SomaFracoes(int x-1);
+    }
+
+
 }
